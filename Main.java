8b2468e7e62a2e9736f22d1827bbd1ef938d8129
victormendesferreira-droid <victor.mendesferreira@ufrpe.br>
2026-09-19 21:06:53 -0300

    Add files via upload
    criação da classe atração

---
+++ Main.java
@@ -0,0 +1,39 @@
+public class Atracoes {
+    private String nome;
+    private int capacidadeMaxima;
+    private int alturaMinima;
+    private int idadeMinima;
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+        }
+    public int getAlturaMinima() {
+        return alturaMinima;
+    }
+    public int getIdadeMinima() {
+        return idadeMinima;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCapacidadeMaxima(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+    public void setAlturaMinima(int alturaMinima) {
+        this.alturaMinima = alturaMinima;
+    }
+    public void setIdadeMinima(int idadeMinima) {
+        this.idadeMinima = idadeMinima;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
