08ec6d2d7c475895b57390374b919c427417a7c1
victormendesferreira-droid <victor.mendesferreira@ufrpe.br>
2026-09-22 00:57:03 -0300

    classe abstrata completa

---
+++ src/Atracoes/ControleAcesso.java
@@ -0,0 +1,44 @@
+package Atracoes;
+
+import java.time.LocalDate;
+
+class ControleAcesso {
+    public boolean registrarAcesso(Visitante visitante,Ingresso ingresso,Atracao atracao) {
+        // Verifica se o ingresso pertence ao visitante
+        if (ingresso.getVisitante() != visitante) {
+            return false;
+        }
+        // Verifica se o ingresso está válido
+        if (!ingresso.valido(LocalDate.now())) {
+            return false;
+        }
+        // Verifica idade e altura
+        if (!atracao.podeEntrar(visitante)) {
+            return false;
+        }
+
+        // Se estiver lotada, coloca na fila virtual
+        if (!atracao.entrar()) {
+            atracao.getFilaVirtual().adicionar(visitante);
+            return false;
+        }
+        return true;
+    }
+
+    // consulta a ocupação atual da atração em tempo real
+    public int consultarOcupacao(Atracao atracao) {
+        return atracao.getOcupacaoAtual();
+    }
+
+    public boolean estaLotada(Atracao atracao) {
+        return atracao.getOcupacaoAtual() >= atracao.getCapacidadeMaxima();
+    }
+
+    public void finalizarUso(Visitante visitante,Atracao atracao) {
+        atracao.sair();// Libera uma vaga
+        if (!atracao.getFilaVirtual().estaVazia()) {//verifica se a fila virtual não está vazia
+            Visitante proximo = atracao.getFilaVirtual().proximo();
+            atracao.entrar();
+        }
+    }
+}
