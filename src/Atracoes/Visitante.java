cc253e1447adcbbb9a992bcd0f3237321257b72c
victormendesferreira-droid <victor.mendesferreira@ufrpe.br>
2026-09-22 01:10:46 -0300

    classe abstrata completa

--- src/Atracoes/Visitante.java
+++ src/Atracoes/Visitante.java
@@ -1,22 +1,25 @@
 package Atracoes;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 class Visitante {
     private String nome;
-    private int idade;
+    private LocalDate dataNascimento;
     private int altura;
 
-    public Visitante(String nome, int idade, int altura) {
+    public Visitante(String nome, LocalDate dataNascimento, int altura) {
         this.nome = nome;
-        this.idade = idade;
+        this.dataNascimento = dataNascimento;
         this.altura = altura;
     }
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
-    public int getIdade() { return idade; }
-    public void setIdade(int idade) { this.idade = idade; }
-
+    public int getIdade() { return Period.between(dataNascimento, LocalDate.now()).getYears(); }
+    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+    
     public int getAltura() { return altura; }
     public void setAltura(int altura) { this.altura = altura; }
 }
