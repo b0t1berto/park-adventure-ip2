f47daa01c476fd326f3bc0c09802e37099ac75b4
victormendesferreira-droid <victor.mendesferreira@ufrpe.br>
2026-09-22 00:54:15 -0300

    classe abstrata completa

---
+++ src/Atracoes/FilaVirtual.java
@@ -0,0 +1,26 @@
+package Atracoes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class FilaVirtual {
+    private Queue<Visitante> fila;//usa uma fila para gerenciar os visitantes que estão esperando para entrar na atração
+    public FilaVirtual() {//estilo primeiro a entrar, primeiro a sair (FIFO)
+        fila = new LinkedList<>();//cria uma fila virtual usando o LinkedList
+    }
+    public void adicionar(Visitante visitante) {
+        fila.add(visitante);
+    }
+
+    public Visitante proximo() {
+        return fila.poll();
+    }
+
+    public int tamanho() {
+        return fila.size();
+    }
+
+    public boolean estaVazia() {
+        return fila.isEmpty();
+    }
+}
