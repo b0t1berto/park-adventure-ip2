9ce263b6b8d4f6a535c6f415da735830bc922f64
victormendesferreira-droid <victor.mendesferreira@ufrpe.br>
2026-09-22 00:35:24 -0300

    classe abstrata completa

--- src/Atracoes/Infantil.java
+++ src/Atracoes/Infantil.java
@@ -1,7 +1,7 @@
 package Atracoes;
 
-public class Infantil extends Atracoes {
-    public Infantil(String nome, int capacidadeMaxima, int alturaMinima, int idadeMinima) {
-        super(nome, capacidadeMaxima, alturaMinima, idadeMinima);
+class Infantil extends Atracao {
+    public Infantil(String nome, int capacidadeMaxima, int alturaMinima, int idadeMinima, Operador operador, FilaVirtual filaVirtual) {
+        super(nome, capacidadeMaxima, alturaMinima, idadeMinima, operador, filaVirtual);
     }
 }
