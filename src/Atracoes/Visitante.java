e9b2c963d48995ec587b35636cd7c02326cac9b6
victormendesferreira-droid <victor.mendesferreira@ufrpe.br>
2026-09-22 00:45:25 -0300

    classe abstrata completa
    Refactored Visitante class by simplifying getter and setter methods.

--- src/Atracoes/Visitante.java
+++ src/Atracoes/Visitante.java
@@ -1,35 +1,22 @@
 package Atracoes;
 
-public class Visitante {
+class Visitante {
     private String nome;
     private int idade;
     private int altura;
 
-    public Visitante(String nome,int idade, int altura) {
+    public Visitante(String nome, int idade, int altura) {
         this.nome = nome;
         this.idade = idade;
         this.altura = altura;
     }
 
-    // Getters and Setters
-    public String getNome() {
-        return nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public int getAltura() {
-        return altura;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public void setAltura(int altura) {
-        this.altura = altura;
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
+    public int getIdade() { return idade; }
+    public void setIdade(int idade) { this.idade = idade; }
+
+    public int getAltura() { return altura; }
+    public void setAltura(int altura) { this.altura = altura; }
 }
-
